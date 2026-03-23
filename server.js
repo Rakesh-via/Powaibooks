9417cace18331ec9055d7eb41828f4732cc0df19
Via Agent <via@e2b.dev>
2026-03-23 12:32:54 +0000

    Revert "Revert "feat: add direct goodreads links for books""
    This reverts commit 98c55e87cc583af08f2d620e360b082abdb8066d.

--- server.js
+++ server.js
@@ -13,6 +13,22 @@ const categoryConfigs = {
   physics: {
     title: 'Physics',
     subjects: ['physics', 'quantum_physics', 'astrophysics'],
+    curatedWorks: [
+      { title: 'A Brief History of Time', author: 'Stephen Hawking' },
+      { title: 'The Elegant Universe', author: 'Brian Greene' },
+      { title: 'Six Easy Pieces', author: 'Richard P. Feynman' },
+      { title: 'The Fabric of the Cosmos', author: 'Brian Greene' },
+      { title: 'Seven Brief Lessons on Physics', author: 'Carlo Rovelli' },
+      { title: 'The Order of Time', author: 'Carlo Rovelli' },
+    ],
+    preferredTitles: [
+      'A Brief History of Time',
+      'The Elegant Universe',
+      'Six Easy Pieces',
+      'The Fabric of the Cosmos',
+      'Seven Brief Lessons on Physics',
+      'The Order of Time',
+    ],
   },
   'ancient-india-history': {
     title: 'History of India',
@@ -24,6 +40,14 @@ const categoryConfigs = {
       { title: 'India: A History', author: 'John Keay' },
       { title: 'Early India', author: 'Romila Thapar' },
       { title: 'The Penguin History of Early India', author: 'Romila Thapar' },
+    ],
+    preferredTitles: [
+      'The Golden Road',
+      'The Anarchy',
+      'The Wonder That Was India',
+      'India: A History',
+      'Early India',
+      'The Penguin History of Early India',
     ],
   },
   'time-management': {
@@ -39,7 +63,55 @@ const categoryConfigs = {
       { title: 'Atomic Habits', author: 'James Clear' },
       { title: 'The 7 Habits of Highly Effective People', author: 'Stephen R. Covey' },
     ],
+    preferredTitles: [
+      'Deep Work',
+      'Getting Things Done',
+      'Essentialism',
+      'Four Thousand Weeks',
+      'Make Time',
+      '168 Hours',
+      'Atomic Habits',
+      'The 7 Habits of Highly Effective People',
+    ],
   },
+};
+
+const titleAliases = {
+  Golden Road: 'The Golden Road',
+  India: 'India: A History',
+  'Make time': 'Make Time',
+  'The wonder that was India': 'The Wonder That Was India',
+};
+
+
+const goodreadsLinks = {
+  'A Brief History of Time|Stephen Hawking': 'https://www.goodreads.com/book/show/3869.A_Brief_History_of_Time',
+  'The Elegant Universe|Brian Greene': 'https://www.goodreads.com/book/show/8049273-the-elegant-universe',
+  'Six Easy Pieces|Richard P. Feynman': 'https://www.goodreads.com/book/show/5553.Six_Easy_Pieces',
+  'The Fabric of the Cosmos|Brian Greene': 'https://www.goodreads.com/book/show/22435.The_Fabric_of_the_Cosmos',
+  'Seven Brief Lessons on Physics|Carlo Rovelli': 'https://www.goodreads.com/book/show/25734172-seven-brief-lessons-on-physics',
+  'The Order of Time|Carlo Rovelli': 'https://www.goodreads.com/book/show/36442813-the-order-of-time',
+  'The Golden Road|William Dalrymple': 'https://www.goodreads.com/book/show/201608148-the-golden-road',
+  'The Anarchy|William Dalrymple': 'https://www.goodreads.com/book/show/42972023-the-anarchy',
+  'The Wonder That Was India|A. L. Basham': 'https://www.goodreads.com/book/show/631246.The_Wonder_That_Was_India',
+  'India: A History|John Keay': 'https://www.goodreads.com/book/show/134694389-india',
+  'Early India|Romila Thapar': 'https://www.goodreads.com/book/show/276117.Early_India',
+  'The Penguin History of Early India|Romila Thapar': 'https://www.goodreads.com/book/show/60799304-the-penguin-history-of-early-india',
+  'Deep Work|Cal Newport': 'https://www.goodreads.com/book/show/25744928-deep-work',
+  'Getting Things Done|David Allen': 'https://www.goodreads.com/book/show/1633.Getting_Things_Done',
+  'Essentialism|Greg McKeown': 'https://www.goodreads.com/book/show/19776547-essentialism',
+  'Four Thousand Weeks|Oliver Burkeman': 'https://www.goodreads.com/book/show/54785515-four-thousand-weeks',
+  'Make Time|Jake Knapp': 'https://www.goodreads.com/book/show/37880811-make-time',
+  '168 Hours|Laura Vanderkam': 'https://www.goodreads.com/book/show/7847359-168-hours',
+  'Atomic Habits|James Clear': 'https://www.goodreads.com/book/show/40121378-atomic-habits',
+  'The 7 Habits of Highly Effective People|Stephen R. Covey': 'https://www.goodreads.com/book/show/36072.The_7_Habits_of_Highly_Effective_People',
+};
+
+const getCanonicalTitle = (title) => titleAliases[title] || title;
+
+const getGoodreadsLink = (title, author) => {
+  const canonicalTitle = getCanonicalTitle(title);
+  return goodreadsLinks[`${canonicalTitle}|${author}`] || goodreadsLinks[`${title}|${author}`] || buildBookLink(canonicalTitle, author);
 };
 
 const pickDescription = (work, category) => {
@@ -66,7 +138,7 @@ const normalizeBook = (work, category) => ({
   rating: Math.max(3.8, Math.min(5, ((work.ratings_average || 4.2) + (work.want_to_read_count ? 0.2 : 0)).toFixed(1))),
   year: work.first_publish_year || null,
   sourceKey: `${category}:${work.key}`,
-  externalUrl: buildBookLink(work.title, work.authors?.[0]?.name || 'Unknown author'),
+  externalUrl: getGoodreadsLink(work.title, work.authors?.[0]?.name || 'Unknown author'),
   externalSource: 'Goodreads',
 });
 
@@ -101,7 +173,7 @@ function normalizeSearchDoc(doc, category) {
     rating: Math.max(3.9, Math.min(5, Number((((doc.ratings_average || 4.2) + ((doc.ratings_count || 0) > 20 ? 0.2 : 0))).toFixed(1)))),
     year: doc.first_publish_year || null,
     sourceKey: `${category}:search:${doc.key}`,
-    externalUrl: buildBookLink(doc.title, doc.author_name?.[0] || 'Unknown author'),
+    externalUrl: getGoodreadsLink(doc.title, doc.author_name?.[0] || 'Unknown author'),
     externalSource: 'Goodreads',
   };
 }
@@ -155,9 +227,20 @@ async function fetchCategoryBooks(category) {
     books = books.filter((book) => !bannedPhrases.some((phrase) => book.title.toLowerCase().includes(phrase)));
   }
 
-  books = books
-    .sort((a, b) => b.rating - a.rating || (b.year || 0) - (a.year || 0))
-    .slice(0, 8);
+  if (config.preferredTitles?.length) {
+    const preferredBooks = config.preferredTitles
+      .map((preferredTitle) => books.find((book) => getCanonicalTitle(book.title) === preferredTitle))
+      .filter(Boolean);
+    const seen = new Set(preferredBooks.map((book) => book.sourceKey));
+    const remainingBooks = books
+      .filter((book) => !seen.has(book.sourceKey))
+      .sort((a, b) => b.rating - a.rating || (b.year || 0) - (a.year || 0));
+    books = [...preferredBooks, ...remainingBooks].slice(0, 8);
+  } else {
+    books = books
+      .sort((a, b) => b.rating - a.rating || (b.year || 0) - (a.year || 0))
+      .slice(0, 8);
+  }
 
   await Promise.all(
     books.map((book) =>
